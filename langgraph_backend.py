85271f4c556816c461be2caa08dc8c69829a6547
shabihosama@gmail.com <shab>
2026-09-24 00:46:18 +0530

    Update LangGraph chatbot with Groq

--- langgraph_backend.py
+++ langgraph_backend.py
@@ -1,29 +1,36 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage
-from langchain_openai import ChatOpenAI
+from langchain_groq import ChatGroq
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = ChatOpenAI()
+# Groq LLM
+llm = ChatGroq(
+    model="openai/gpt-oss-20b",
+    temperature=0
+)
 
 class ChatState(TypedDict):
     messages: Annotated[list[BaseMessage], add_messages]
 
 def chat_node(state: ChatState):
-    messages = state['messages']
+    messages = state["messages"]
     response = llm.invoke(messages)
     return {"messages": [response]}
 
 # Checkpointer
 checkpointer = InMemorySaver()
 
+# Build graph
 graph = StateGraph(ChatState)
+
 graph.add_node("chat_node", chat_node)
 graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
 
+# Compile
 chatbot = graph.compile(checkpointer=checkpointer)
